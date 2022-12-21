4fbddfbaeca716a03a38eccd7925b5f6196cd05c
StarsbySea <66008060+StarsbySea@users.noreply.github.com>
2022-12-21 14:40:16 +0000

    Update fjjkm.js

--- fjjkm.js
+++ fjjkm.js
@@ -13,14 +13,25 @@ hostname = fjjkm1.nebulabd.cn
 
 var body = $response.body;
 var objc = body;
-
-objc["Response"]["Data"]["NucleicAcidInfoList"] = {
-    "ResultTime" : "2022-07-21 01:31:25",
-    "SamplingTime" : "2022-07-20 19:04:46",
-    "CheckOrgName" : "三明市中西医结合医院",
-    "Result" : "2",
-    "CardName" : "魏*云"
-};
-
-body = objc
+objc["Response"]["Data"]["CaidStatus"] = 2
+objc["Response"]["Data"]["NucleicAcidInfoList"] = [
+        {
+          "Result": "2",
+          "TubeType": 1,
+          "CrowdType": 19,
+          "ResultTime": "2022-12-20 21:20:29",
+          "SamplingTime": "2022-12-20 16:37:21",
+          "CheckOrgName": "宁德市闽东医院",
+          "CardName": "*晓"
+        },
+        {
+          "Result": "2",
+          "TubeType": 1,
+          "CrowdType": 9,
+          "ResultTime": "2022-12-14 05:37:55",
+          "SamplingTime": "2022-12-13 20:25:30",
+          "CheckOrgName": "福安市疾控PCR实验室",
+          "CardName": "*晓"
+        }];
+body = JSON.stringify(objc);
 $done({ body });
